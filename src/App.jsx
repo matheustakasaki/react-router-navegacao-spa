efbc36fdef0eb256c40d23ff5720a4aee48c9ff7
matheustakasaki <matheus8antunes@gmail.com>
2022-02-23 23:38:57 -0300

    instalando dependencias, criando gitignore para a pasta node modules e renderizando componentes condicionalmente com if else

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import './assets/css/base/base.css'
-
+import Home from './paginas/Home'
+import Sobre from './paginas/Sobre'
 function App() {
-  return (
-  <>
 
-  </>
+  const Router = () => {
+    const location = window.location.pathname
+    if (location === '/sobre') {
+      return <Sobre></Sobre>
+    } else {
+      return <Home />
+    }
+  }
+  return (
+    <>
+      {Router()}
+    </>
   )
 }
 
